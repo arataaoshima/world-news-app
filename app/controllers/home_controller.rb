12266f7b60fde494746fdccd772d3ffed2c7a228
ARATA <newbluelight1031@gmail.com>
2019-02-12 04:30:16 +0000

    change styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   
   def top
-    @posts = Post.search(params[:search]).paginate(page: params[:page], per_page: 3)
+    @posts = Post.search(params[:search]).paginate(page: params[:page], per_page: 6)
   end
+  
+  
 end
