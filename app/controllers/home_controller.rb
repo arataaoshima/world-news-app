ebaa5e2722afc93108fe8c23cb6f43bca4ab748d
Arata <newbluelight1031@gmail.com>
2020-02-22 23:40:50 -0800

    change order top

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
-  
+
   def top
-    @posts = Post.search(params[:search]).paginate(page: params[:page], per_page: 6)
+    @posts = Post.search(params[:search]).paginate(page: params[:page], per_page: 6).order(id: "DESC")
   end
-  
-  
+
+
 end
